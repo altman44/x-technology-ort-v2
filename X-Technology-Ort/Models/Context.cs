66e4c1beb3052128435a1d10c2ac8fb4ebc82a08
nikoaltman505@gmail.com <nikoaltman505@gmail.com>
2020-11-04 22:48:08 -0300

    first commit

---
+++ X-Technology-Ort/Models/Context.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace X_Technology_ORT.Models
+{
+    public class Context : DbContext
+    {
+        public virtual DbSet<Billing> Billings { get; set; }
+        public virtual DbSet<OrderDetail> OrdersDetails { get; set; }
+        public virtual DbSet<OrderHeader> OrdersHeader { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<Shipping> Shippings { get; set;}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(
+                    "Data Source=localhost; Initial Catalog=XTechnologyORT; user id=SA; pwd=XXX!"
+                );
+            }
+        }
+    }
+}
---
+++ X-Technology-Ort/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace X_Technology_ORT.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
